ac79c76c2d8f33cccf24ab2aea921291864f74a1
phuocn0302 <phuocnguyen230302@gmail.com>
2025-04-23 00:25:13 +0700

    fix (none-ls): remove beautysh config

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -6,7 +6,7 @@ return {
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.beautysh,
+        --null_ls.builtins.formatting.beautysh,
       },
     })
     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
