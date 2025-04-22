bbd03996389d8d3c30dedb108e8b67042f287b1c
phuocn0302 <phuocnguyen230302@gmail.com>
2025-04-23 00:21:44 +0700

    feat: add markdown rendering extension

---
+++ lua/plugins/md.lua
@@ -0,0 +1,10 @@
+return {
+  "MeanderingProgrammer/render-markdown.nvim",
+  dependencies = { "nvim-treesitter/nvim-treesitter", "echasnovski/mini.nvim" }, -- if you use the mini.nvim suite
+  ---@module 'render-markdown'
+  ---@type render.md.UserConfig
+  opts = {},
+  config = function()
+    require("render-markdown").enable()
+  end,
+}
