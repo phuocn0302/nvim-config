9ce62d564ebabac53c38a2a5ec85826bc3d1bef8
phuocn0302 <phuocnguyen230302@gmail.com>
2025-10-19 22:42:12 +0700

    feat: remove mason-lsp setup_handlers api v2+ remove it

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -9,6 +9,7 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		config = function()
 			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "ts_ls", "pyright" },
 				automatic_installation = true,
 			})
 		end,
@@ -21,17 +22,35 @@ return {
 		},
 		config = function()
 			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			local mason_lspconfig = require("mason-lspconfig")
 
-			require("mason-lspconfig").setup_handlers({
-				function(server_name)
-					require("lspconfig")[server_name].setup({
+			local default_handler = function(server_name)
+				vim.lsp.config[server_name] = vim.lsp.config[server_name] or {}
+				vim.lsp.config[server_name] = vim.tbl_deep_extend("force", vim.lsp.config[server_name], {
+					capabilities = capabilities,
+				})
+				vim.lsp.start(vim.lsp.config[server_name])
+			end
+
+			local handlers = {
+				["lua_ls"] = function()
+					vim.lsp.config.lua_ls = vim.tbl_deep_extend("force", vim.lsp.config.lua_ls or {}, {
 						capabilities = capabilities,
+						settings = {
+							Lua = { diagnostics = { globals = { "vim" } } },
+						},
 					})
+					vim.lsp.start(vim.lsp.config.lua_ls)
 				end,
-			})
+			}
+
+			for _, server_name in ipairs(mason_lspconfig.get_installed_servers()) do
+				(handlers[server_name] or default_handler)(server_name)
+			end
 
 			vim.keymap.set({ "n", "v" }, "<space>ca", vim.lsp.buf.code_action, {})
 			vim.keymap.set("n", "<leader>e", vim.diagnostic.open_float, { desc = "Show diagnostics in float" })
 		end,
 	},
 }
+
